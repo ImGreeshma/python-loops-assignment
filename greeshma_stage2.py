d41817d6010b49e1efe98968583bd16d06949822
ImGreeshma <greeshma.qa2ux@gmail.com>
2026-02-20 08:25:31 +0530

    Add Weekly Temperature Analysis Assignment File

---
+++ greeshma_stage2.py
@@ -0,0 +1,29 @@
+num1, num2, operator =  input("Enter number1 number2 and operator(+,-,*,/) seperated by comma:").split(",")
+num1 = int(num1.strip())
+num2 = int(num2.strip())
+operator = operator.strip()
+
+if(operator == '+'):
+    result = num1 + num2
+elif(operator == '-'):
+    result = num1 - num2
+elif(operator == "*"):
+    result = num1 * num2
+elif(operator == '/'):
+    if(num2 == 0):
+        print("Division By Zero")
+        exit()
+    else:
+        result = num1 / num2
+else:
+    print ("invalid Operator")
+    exit()
+
+print(f"Result = {result} ")
+
+if result > 0 :
+    print ("Positive")
+elif result < 0 :
+    print ("Negative")
+elif result  == 0:
+    print ("Zero")
